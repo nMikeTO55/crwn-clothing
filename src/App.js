4344741896c8dbfc0df53d91d26bcefd2781d379
Mike <mike.j.parish@gmail.com>
2025-01-22 14:09:04 -0500

    79 Category Item Component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
 import "./categories.styles.scss";
 import CategoryItem from './components/category-item/category-item.component.jsx';
+import ProductList  from "./components/data/data.products.jsx";
 
 const  App = ()=> {
 
-  const CategoriesData = [
-    {
-      "id": 1,
-      "title": "Hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
+  <ProductList />
 
   return (
     <div className='categories-container'>
-      {CategoriesData.map((CategoriesData) => (
-        <CategoryItem key={CategoriesData.id} category={CategoriesData}/>
+      {ProductList.map((product) => (
+        <CategoryItem key={product.id} category={product}/>
      ))}
    </div>
   );
