7248aa26f03deccf0b6b6b123ec58a4c92f59e18
Mike <mike.j.parish@gmail.com>
2025-01-23 11:51:40 -0500

    84 Setting up Homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import CategoryItem from './components/category-item/category-item.component.jsx';
-import ProductList  from "./components/data/data.products.jsx";
-import Menu         from "./components/menus/menus.main.jsx";
+//import CategoryItem from './components/category-item/category-item.component.jsx';
+//import ProductList  from "./components/data/data.products.jsx";
+//import Menu         from "./components/menus/menus.main.jsx";
+import Home from './routes/home/home.component.jsx';
 
 const  App = ()=> {
 
   return (
-    <Menu />
+    <Home />
   )
 }
 
