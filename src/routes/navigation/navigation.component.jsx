24b3f698124edbab68b972d7ff1ed3c9b5a6002b
Mike <mike.j.parish@gmail.com>
2025-01-28 14:08:12 -0500

    88 styling for nav

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,17 @@
 import {Outlet, Link} from 'react-router-dom';
 import {Fragment} from 'react';
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+import './navigation.styles.scss';
 
 const Navigation = ()=>{
   return (
     <Fragment>
       <div className='navigation'>
-        <Link classNaem='logo-container' to='/'>
-          <div>Logo</div>
+        <Link className='logo-container' to='/'>
+          <div><CrwnLogo className='logo'/></div>
         </Link>
-        <div className='nav-link-container'>
-          <Link classname='nav-link' to='/shop/'>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop/'>
           SHOP
           </Link>
         </div>
