561d2afd8b82f77368050232839f7bd6338d6671
Mike <mike.j.parish@gmail.com>
2025-02-12 15:08:18 -0500

    111 finalizing auth listener

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,13 @@
 import { Outlet, Link } from "react-router-dom";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/user.context.jsx";
+import { signOutUser } from "../../utils/firebase/firebase.utils.js";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <Fragment>
       <div className="navigation">
@@ -16,9 +20,16 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop/">
             SHOP
           </Link>
-          <Link className="nav-link" to="/auth/">
-            SIGN IN PAGE
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>
+              {" "}
+              SIGN OUT{" "}
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth/">
+              SIGN IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
