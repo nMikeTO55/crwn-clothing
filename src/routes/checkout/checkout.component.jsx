42b8ac48e8da674536534326f3fa0746ab1b223c
Mike <mike.j.parish@gmail.com>
2025-02-26 13:45:18 -0500

    checkout item pt3

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -6,8 +6,8 @@ import CheckoutItem from "../../components/checkout-item/checkout-item.component
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-  const { cartItems, addItemToCart, removeItemFromCart } =
-    useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="checkout-container">
       <div className="checkout-header">
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,6 +3,7 @@ import { Outlet, Link } from "react-router-dom";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { UserContext } from "../../contexts/user.context.jsx";
 import { CartContext } from "../../contexts/cart.context.jsx";
 
@@ -19,20 +20,19 @@ const Navigation = () => {
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>
-            <CrwnLogo className="logo" />
-          </div>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
-          <Link className="nav-link" to="/shop/">
+          <Link className="nav-link" to="/shop">
             SHOP
           </Link>
+          
           {currentUser ? (
             <span className="nav-link" onClick={signOutUser}>
               SIGN OUT
             </span>
           ) : (
-            <Link className="nav-link" to="/auth/">
+            <Link className="nav-link" to="/auth">
               SIGN IN
             </Link>
           )}
